2b0049f527d3470f2dea07625680b1d2f2c08725
dmaruyama-51 <0501awasuram@gmail.com>
2025-01-04 18:36:53 +0900

    func_loaderのテストを追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,7 @@ sys.path.insert(0, str(functions_dir))
 # func_scraper/utilsディレクトリをPythonパスに追加
 func_scraper_utils = functions_dir / "func_scraper"
 sys.path.insert(0, str(func_scraper_utils))
+
+# sharedディレクトリをPythonパスに追加
+shared_dir = functions_dir / "shared"
+sys.path.insert(0, str(shared_dir))
