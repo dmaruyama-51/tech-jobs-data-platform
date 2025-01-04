b5461195d472b62479869d8cb151fd146ad856ca
dmaruyama-51 <0501awasuram@gmail.com>
2025-01-04 21:18:32 +0900

    sqlfluff fix

--- dbt/models/staging/stg_joblist.sql
+++ dbt/models/staging/stg_joblist.sql
@@ -11,7 +11,6 @@ with source as (
 
 staged as (
     select
-        to_hex(md5(detail_link)) as job_id,
         detail_link,
         listing_start_date,
         monthly_salary,
@@ -22,14 +21,16 @@ staged as (
         job_content,
         required_skills,
         preferred_skills,
+        rate_of_work,
+        job_title,
+        to_hex(md5(detail_link)) as job_id,
         split(programming_language, '\n') as programming_language,
         split(tool, '\n') as tool,
         split(framework, '\n') as framework,
-        rate_of_work,
-        safe_cast(regexp_extract(number_of_recruitment_interviews, r'(\d+)') as int64) as number_of_recruitment_interviews,
+        safe_cast(regexp_extract(number_of_recruitment_interviews, r'(\d+)') as int64)
+            as number_of_recruitment_interviews,
         safe_cast(regexp_extract(number_of_days_worked, r'週(\d+)日') as int64) as number_of_days_worked,
-        safe_cast(regexp_extract(number_of_applicants, r'(\d+)') as int64) as number_of_applicants,
-        job_title
+        safe_cast(regexp_extract(number_of_applicants, r'(\d+)') as int64) as number_of_applicants
     from source
 )
 
